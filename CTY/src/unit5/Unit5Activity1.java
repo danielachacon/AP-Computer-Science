e11d405472092235ae768ea023bd6c2237e25c5c
danielachacon <68338389+danielachacon@users.noreply.github.com>
2023-03-21 18:11:44 -0400

    finished unit 5 activity 1 + comments

--- CTY/src/unit5/Unit5Activity1.java
+++ CTY/src/unit5/Unit5Activity1.java
@@ -3,10 +3,12 @@ package unit5;
 import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math;
-// 1a) 8
-// 1b) 0
+// 1a) 2 constants - e and pi.
+// 1b) 55
+// 1c) 26
+// 1d) 11
 
-public class Unit5Activity1 {	//done except 1
+public class Unit5Activity1 {
 
 	public static void main(String[] args)
 	{
@@ -27,27 +29,27 @@ public class Unit5Activity1 {	//done except 1
 		//3a) The sequences of numbers each time were different
 		//3b) When random is first called each run, it creates a single new pseudorandom-number generator.
 		//	  This causes it to different each time.
-		for(int i = 0; i < 20; i++)
+		for(int i = 0; i < 20; i++)	//Displays 20 different random integers ranging from 1-5
 			System.out.print((int) (Math.random() * 5) + 1 + " ");
 		
 		System.out.println();
 		
 		//4. Random int 100 times using Random Class and count occurrences
 		
-		Random rand = new Random(1);
+		Random rand = new Random(1);	//Same seed (1) each time leads to the same numbers generated each time
 		int number;
 		int count = 0;
-		int array[] = new int[23];
+		int array[] = new int[23];	//Stores occurrences of each element of the set {6,10,14,18,22}
 		
-		while(count < 100)
+		while(count < 100)	//Repeat until we get 100 total occurrences of the set {6,10,14,18,22}.
 		{
 			number = rand.nextInt(23);
 			switch(number)
 			{
 			case 6:
-				array[6]++;
-				System.out.print(number + " ");
-				count++;
+				array[6]++; //Adds to the amount of occurrences for the specific number
+				System.out.print(number + " ");	//Prints the number generated
+				count++;	//Adds to the total amount of occurrences of all numbers in the set {6,10,14,18,22}
 				break;
 			case 10:
 				array[10]++;
@@ -88,7 +90,7 @@ public class Unit5Activity1 {	//done except 1
 		x = input.nextDouble();
 		System.out.println("Enter a number: ");
 		y = input.nextDouble();
-		
+		System.out.println((int) (Math.floor((x/y) * Math.pow(10,0) + 0.5) / Math.pow(10, 0)));
 		System.out.println(Math.floor((x/y) * Math.pow(10,1) + 0.5) / Math.pow(10, 1));
 		System.out.println(Math.floor((x/y) * Math.pow(10,2) + 0.5) / Math.pow(10, 2));
 		System.out.println(Math.floor((x/y) * Math.pow(10,3) + 0.5) / Math.pow(10, 3));
