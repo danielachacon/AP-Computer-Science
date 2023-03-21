0ee42809ecabd7457ec75dbe01be3134cded459b
danielachacon <68338389+danielachacon@users.noreply.github.com>
2023-03-21 15:36:01 -0400

    commented arraytools and arraytools2

--- CTY/src/unit6/ArrayTools.java
+++ CTY/src/unit6/ArrayTools.java
@@ -2,21 +2,38 @@ package unit6;
 
 import java.util.Arrays;
 
+/**
+ * This class stores methods which receive an array of characters/integers/doubles
+ * and can compute the minimum, maximum, minimum/maximum positions, and averages of the array.
+ * 
+ * @author Daniel Achacon
+ */
+
 public class ArrayTools {	//Check
 	
+	/**
+	 * @param array of characters
+	 * @return minimum (smallest) element of the array
+	 */
 	public static char minimum(char array[])
 	{
-		char minimum = array[0];
+		char minimum = array[0];					//Predetermines first element as the temporary minimum
 		
-		for(int i = 1; i < array.length; i++) 
+		for(int i = 1; i < array.length; i++) 	//Traverses through all elements after the first element and checks if it is less than the current minimum 
 		{
-			if(array[i] < minimum)
-				minimum = array[i];
+			if(array[i] < minimum)			//Checks if current element is less than the current minimum 
+				minimum = array[i];			//If less than the minimum, then the current element becomes the new minimum
 		}
 		
 		return minimum;
 	}
 	
+	/**
+	 * @param array of integers
+	 * @return minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static int minimum(int array[])
 	{
 		int minimum = array[0];
@@ -30,6 +47,12 @@ public class ArrayTools {	//Check
 		return minimum;
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static double minimum(double array[])
 	{
 		double minimum = array[0];
@@ -45,19 +68,29 @@ public class ArrayTools {	//Check
 	
 	/*__________________________________________________________*/
 	
-	
+	/**
+	 * @param array of characters
+	 * @return maximum (largest) element of the array
+	 */
 	public static char maximum( char array[])
 	{
-		char maximum = 0;
+		char maximum = 0;					//Predetermines first element as the temporary maximum
 		
-		for(int i = 0; i < array.length; i++) 
+		for(int i = 0; i < array.length; i++) //Traverses through all elements after the first element and checks if it is greater than the current maximum
 		{
-			if(array[i] > maximum)
-				maximum = array[i];
+			if(array[i] > maximum)		//Checks if current element is greater than the current maximum 
+				maximum = array[i];		//If greater than the maximum, then the current element becomes the new maximum
 		}
 		
 		return maximum;
 	}
+	
+	/**
+	 * @param array of integers
+	 * @return maximum (largest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static int maximum( int array[])
 	{
 		int maximum = 0;
@@ -71,6 +104,12 @@ public class ArrayTools {	//Check
 		return maximum;
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return maximum (largest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static double maximum( double array[])
 	{
 		double maximum = 0;
@@ -86,24 +125,33 @@ public class ArrayTools {	//Check
 	
 	/*__________________________________________________________*/
 	
-	
-	public static int minimumAt(char array[])
+	/**
+	 * @param array of characters
+	 * @return position in array of the minimum (smallest) element of the array
+	 */
+	public static int minimumAt(char array[])	
 	{
-		char minimum = array[0];
-		int place = 0;
+		char minimum = array[0]; //Predetermines first element as the temporary minimum
+		int place = 0; //Stores position of the minimum
 		
-		for(int i = 1; i < array.length; i++) 
+		for(int i = 1; i < array.length; i++) //Traverses through all elements after the first element and checks if it is less than the current minimum 
 		{
-			if(array[i] < minimum)
+			if(array[i] < minimum)	//Checks if current element is less than the current minimum  
 			{
-				place = (char) i;
-				minimum = array[i];
+				place = (char) i;	//If less than the minimum, then the current element becomes the new minimum
+				minimum = array[i];	//Position of minimum gets updated
 			}
 		}
 		
 		return place;
 	}
 	
+	/**
+	 * @param array of integers
+	 * @return position in array of the minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as the method above
+	 */
 	public static int minimumAt(int array[])
 	{
 		int minimum = array[0];
@@ -121,6 +169,12 @@ public class ArrayTools {	//Check
 		return place;
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return position in array of the minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as the method above
+	 */
 	public static int minimumAt(double array[])
 	{
 		double minimum = array[0];
@@ -140,23 +194,33 @@ public class ArrayTools {	//Check
 
 	/*__________________________________________________________*/
 	
+	/**
+	 * @param array of characters
+	 * @return position in array of the maximum (largest) element of the array
+	 */
 	public static int maximumAt(char array[])
 	{
-		char maximum = 0;
-		int place = 0;
+		char maximum = 0;	//Predetermines first element as the temporary maximum
+		int place = 0;	//Stores position of the maximum
 		
-		for(int i = 0; i < array.length; i++) 
+		for(int i = 0; i < array.length; i++) //Traverses through all elements after the first element and checks if it is greater than the current maximum
 		{
-			if(array[i] > maximum)
+			if(array[i] > maximum)	//Checks if current element is greater than the current maximum  
 			{
-				maximum = array[i];
-				place = i;
+				maximum = array[i];	//If greater than the maximum, then the current element becomes the new maximum
+				place = i;			//Position of maximum gets updated
 			}
 		}
 		
 		return place;
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return position in array of the maximum (largest) element of the array
+	 * 
+	 * Uses the same logic as the method above
+	 */
 	public static int maximumAt(double array[])
 	{
 		double maximum = 0;
@@ -174,6 +238,12 @@ public class ArrayTools {	//Check
 		return place;
 	}
 	
+	/**
+	 * @param array of integers
+	 * @return position in array of the maximum (largest) element of the array
+	 * 
+	 * Uses the same logic as the method above
+	 */
 	public static int maximumAt(int array[])
 	{
 		int maximum = 0;
@@ -194,16 +264,26 @@ public class ArrayTools {	//Check
 	
 	/*__________________________________________________________*/
 	
+	/**
+	 * @param array of integers
+	 * @return average of all elements of the array
+	 */
 	public static double average(int array[])
 	{
 		double average = 0;
 		
-		for(int i = 0; i < array.length; i++) 
+		for(int i = 0; i < array.length; i++) // Adds all the elements of the array and stores it in average
 			average += array[i];
 		
-		return average / array.length;
+		return average / array.length;		//Divides the sum by the amount of elements in the array to return the average
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return average of all elements of the array
+	 * 
+	 * Uses the same logic as the method above
+	 */
 	public static double average(double array[])
 	{
 		double average = 0;
--- CTY/src/unit6/ArrayTools2.java
+++ CTY/src/unit6/ArrayTools2.java
@@ -1,20 +1,41 @@
 package unit6;
 import java.util.Arrays;
+
+/**
+ * This class stores methods which receive an array of characters/integers/doubles
+ * and can compute the minimum, maximum, minimum/maximum positions, and averages of the array.
+ * 
+ * This class also stores methods which can search for certain values in the array and return its position, 
+ * sort the array in ascending order, and can tell if two arrays are equal to each other.
+ * 
+ * @author Daniel Achacon
+ */
+
 public class ArrayTools2 
 {
+	/**
+	 * @param array of characters
+	 * @return minimum (smallest) element of the array
+	 */
 	public static char minimum(char array[])
 	{
-		char minimum = array[0];
+		char minimum = array[0];					//Predetermines first element as the temporary minimum
 		
-		for(int i = 1; i < array.length; i++) 
+		for(int i = 1; i < array.length; i++) 	//Traverses through all elements after the first element and checks if it is less than the current minimum 
 		{
-			if(array[i] < minimum)
-				minimum = array[i];
+			if(array[i] < minimum)			//Checks if current element is less than the current minimum 
+				minimum = array[i];			//If less than the minimum, then the current element becomes the new minimum
 		}
 		
 		return minimum;
 	}
 	
+	/**
+	 * @param array of integers
+	 * @return minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static int minimum(int array[])
 	{
 		int minimum = array[0];
@@ -28,6 +49,12 @@ public class ArrayTools2
 		return minimum;
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static double minimum(double array[])
 	{
 		double minimum = array[0];
@@ -43,19 +70,29 @@ public class ArrayTools2
 	
 	/*__________________________________________________________*/
 	
-	
+	/**
+	 * @param array of characters
+	 * @return maximum (largest) element of the array
+	 */
 	public static char maximum( char array[])
 	{
-		char maximum = 0;
+		char maximum = 0;					//Predetermines first element as the temporary maximum
 		
-		for(int i = 0; i < array.length; i++) 
+		for(int i = 0; i < array.length; i++) //Traverses through all elements after the first element and checks if it is greater than the current maximum
 		{
-			if(array[i] > maximum)
-				maximum = array[i];
+			if(array[i] > maximum)		//Checks if current element is greater than the current maximum 
+				maximum = array[i];		//If greater than the maximum, then the current element becomes the new maximum
 		}
 		
 		return maximum;
 	}
+	
+	/**
+	 * @param array of integers
+	 * @return maximum (largest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static int maximum( int array[])
 	{
 		int maximum = 0;
@@ -69,6 +106,12 @@ public class ArrayTools2
 		return maximum;
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return maximum (largest) element of the array
+	 * 
+	 * Uses same logic as method above
+	 */
 	public static double maximum( double array[])
 	{
 		double maximum = 0;
@@ -84,24 +127,33 @@ public class ArrayTools2
 	
 	/*__________________________________________________________*/
 	
-	
-	public static char minimumAt(char array[])
+	/**
+	 * @param array of characters
+	 * @return position in array of the minimum (smallest) element of the array
+	 */
+	public static int minimumAt(char array[])	
 	{
-		char minimum = array[0];
-		char place = 0;
+		char minimum = array[0]; //Predetermines first element as the temporary minimum
+		int place = 0; //Stores position of the minimum
 		
-		for(int i = 1; i < array.length; i++) 
+		for(int i = 1; i < array.length; i++) //Traverses through all elements after the first element and checks if it is less than the current minimum 
 		{
-			if(array[i] < minimum)
+			if(array[i] < minimum)	//Checks if current element is less than the current minimum  
 			{
-				place = (char) i;
-				minimum = array[i];
+				place = (char) i;	//If less than the minimum, then the current element becomes the new minimum
+				minimum = array[i];	//Position of minimum gets updated
 			}
 		}
 		
 		return place;
 	}
 	
+	/**
+	 * @param array of integers
+	 * @return position in array of the minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as the method above
+	 */
 	public static int minimumAt(int array[])
 	{
 		int minimum = array[0];
@@ -119,10 +171,16 @@ public class ArrayTools2
 		return place;
 	}
 	
-	public static double minimumAt(double array[])
+	/**
+	 * @param array of doubles
+	 * @return position in array of the minimum (smallest) element of the array
+	 * 
+	 * Uses same logic as the method above
+	 */
+	public static int minimumAt(double array[])
 	{
 		double minimum = array[0];
-		double place = 0;
+		int place = 0;
 		
 		for(int i = 1; i < array.length; i++) 
 		{
@@ -138,27 +196,37 @@ public class ArrayTools2
 
 	/*__________________________________________________________*/
 	
-	public static char maximumAt(char array[])
+	/**
+	 * @param array of characters
+	 * @return position in array of the maximum (largest) element of the array
+	 */
+	public static int maximumAt(char array[])
 	{
-		char maximum = 0;
-		char place = 0;
+		char maximum = 0;	//Predetermines first element as the temporary maximum
+		int place = 0;	//Stores position of the maximum
 		
-		for(int i = 0; i < array.length; i++) 
+		for(int i = 0; i < array.length; i++) //Traverses through all elements after the first element and checks if it is greater than the current maximum
 		{
-			if(array[i] > maximum)
+			if(array[i] > maximum)	//Checks if current element is greater than the current maximum  
 			{
-				maximum = array[i];
-				place = (char) i;
+				maximum = array[i];	//If greater than the maximum, then the current element becomes the new maximum
+				place = i;			//Position of maximum gets updated
 			}
 		}
 		
 		return place;
 	}
 	
-	public static double maximumAt(double array[])
+	/**
+	 * @param array of doubles
+	 * @return position in array of the maximum (largest) element of the array
+	 * 
+	 * Uses the same logic as the method above
+	 */
+	public static int maximumAt(double array[])
 	{
 		double maximum = 0;
-		double place = 0;
+		int place = 0;
 		
 		for(int i = 0; i < array.length; i++) 
 		{
@@ -172,6 +240,12 @@ public class ArrayTools2
 		return place;
 	}
 	
+	/**
+	 * @param array of integers
+	 * @return position in array of the maximum (largest) element of the array
+	 * 
+	 * Uses the same logic as the method above
+	 */
 	public static int maximumAt(int array[])
 	{
 		int maximum = 0;
@@ -192,16 +266,26 @@ public class ArrayTools2
 	
 	/*__________________________________________________________*/
 	
+	/**
+	 * @param array of integers
+	 * @return average of all elements of the array
+	 */
 	public static double average(int array[])
 	{
-		int average = 0;
+		double average = 0;
 		
-		for(int i = 0; i < array.length; i++) 
+		for(int i = 0; i < array.length; i++) // Adds all the elements of the array and stores it in average
 			average += array[i];
 		
-		return average / array.length;
+		return average / array.length;		//Divides the sum by the amount of elements in the array to return the average
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return average of all elements of the array
+	 * 
+	 * Uses the same logic as the method above
+	 */
 	public static double average(double array[])
 	{
 		double average = 0;
@@ -214,13 +298,25 @@ public class ArrayTools2
 	
 	/*__________________________________________________________*/
 	
+	/**
+	 * @param array of characters
+	 * @param array of characters
+	 * @return boolean (TRUE/FALSE) for if the two arrays are equal to each other
+	 */
 	public boolean equals( char a1[], char a2[] )
 	{
-		if(Arrays.equals(a1,a2)  == true)
+		if(Arrays.equals(a1,a2)  == true)		//Calls Arrays.equals method which compares the two arrays and sees if they are equal. If they are, return true. If not, return false.
 			return true;
 		else
 			return false;
 	}
+	/**
+	 * @param array of integers
+	 * @param array of integers
+	 * @return boolean (TRUE/FALSE) for if the two arrays are equal to each other
+	 * 
+	 * Uses same logic as method above
+	 */
 	public boolean equals( int a1[], int a2[] )
 	{
 		if(Arrays.equals(a1,a2)  == true)
@@ -228,6 +324,13 @@ public class ArrayTools2
 		else
 			return false;
 	}
+	/**
+	 * @param array of doubles
+	 * @param array of doubles
+	 * @return boolean (TRUE/FALSE) for if the two arrays are equal to each other
+	 * 
+	 * Uses same logic as method above
+	 */
 	public boolean equals( double a1[], double a2[] )
 	{
 		if(Arrays.equals(a1,a2)  == true)
@@ -237,43 +340,73 @@ public class ArrayTools2
 	}
 	
 	/*__________________________________________________________*/
-	
+	/**
+	 * @param array of characters
+	 * @param character key to search for in element
+	 * @return position of the key element in array, returns negative value if none
+	 */
 	public int find(char array[], char key)
 	{
-		return Arrays.binarySearch(array,key);
+		return Arrays.binarySearch(array,key);	//Calls Arrays.binarysearch method to find the position of the key in the array
 		
 	}
 	
+	/**
+	 * @param array of integers
+	 * @param integer key to search for in element
+	 * @return position of the key element in array, returns negative value if none
+	 * 
+	 * Uses same logic as method above
+	 */
 	public int find(int array[], int key)
 	{
 		return Arrays.binarySearch(array,key);
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @param double key to search for in element
+	 * @return position of the key element in array, returns negative value if none
+	 * 
+	 * Uses same logic as method above
+	 */
 	public int find(double array[], double key)
 	{
 		return Arrays.binarySearch(array,key);
 	}
 	/*__________________________________________________________*/
-	
+	/**
+	 * @param array of characters
+	 * @return array of characters sorted in ascending order
+	 */
 	public char[] sort(char array[])
 	{
-		Arrays.sort(array);
+		Arrays.sort(array);	//Calls Arrays.sort method to sort array in ascending order then returns the array
 		return array;
 	}
 	
+	/**
+	 * @param array of integers
+	 * @return array of integers sorted in ascending order
+	 * 
+	 * Uses same logic as method above
+	 */
 	public int[] sort( int array[] )
 	{
 		Arrays.sort(array);
 		return array;
 	}
 	
+	/**
+	 * @param array of doubles
+	 * @return array of doubles sorted in ascending order
+	 * 
+	 * Uses same logic as method above
+	 */
 	public double[] sort( double array[] )
 	{
 		Arrays.sort(array);
 		return array;
 	}
-	/*__________________________________________________________*/
-
-
 
 }
