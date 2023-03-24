4215b9062a2f33a32e85c5ca541e9d851026d77a
danielachacon <68338389+danielachacon@users.noreply.github.com>
2023-03-24 19:11:17 -0400

    Commented Unit9Lab2

--- CTY/src/unit9/DeckOfCards.java
+++ CTY/src/unit9/DeckOfCards.java
@@ -1,25 +1,38 @@
 package unit9;
 
 import java.util.Random;
+
+/**
+ * This class models a deck of cards which starts unshuffled 
+ * and contains methods that shuffle the deck and displays it and unshuffle it again
+ * @author Daniel Achacon
+ */
 public class DeckOfCards 
 {
-	private String unshuffledCards[][] = new String[52][2];	// 0 = value 1 = suit
+	private String unshuffledCards[][] = new String[52][2];	// column 0 stores value,  column 1 stores suit
 	
 	private String suits[] = {"clubs", "diamonds", "hearts", "spades"};
 	private String value[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 	
+	/**
+	 * Displays Cards from top of deck to bottom of deck
+	 */
 	public void displayDeck()
 	{
 		for(int i = 0; i < unshuffledCards.length; i++)
-			System.out.println(unshuffledCards[i][0] + " " + unshuffledCards[i][1]);
+			System.out.println(unshuffledCards[i][0] + " " + unshuffledCards[i][1]);	//Each card is 1 row with 2 attributes, Goes through each row and prints value and suit for each card
 	}
 	
+	/**
+	 * Shuffles the deck to have random order
+	 */
 	public void shuffleDeck()
 	{
 		Random rand = new Random();
 		int randomIndex;
 		String tempSuit, tempValue;
 		
+		//Picks a random card in the deck and swaps it with ith card in the deck each iteration
 		for(int i = unshuffledCards.length - 1; i >= 1; i--)
 		{
 			randomIndex = rand.nextInt(i + 1);
@@ -34,6 +47,9 @@ public class DeckOfCards
 		}
 	}
 	
+	/**
+	 * Unshuffles the deck, makes each card in order
+	 */
 	public void unshuffleDeck()
 	{
 		int valueCount = 0;
@@ -41,17 +57,25 @@ public class DeckOfCards
 		
 		for(int i = 0; i < 52; i++)
 		{
+			//Goes through each card and fixes both attributes in correct order
 			unshuffledCards[i][0] = value[valueCount];
 			unshuffledCards[i][1] = suits[suitCount];
-			if((valueCount + 1) % 13 == 0)
+			
+			//Iterates through every value before changing suit (ex: Clubs 1-King, then Diamonds 1-King)
+			if((valueCount + 1) % 13 == 0)	//If card is unshuffled for every value for 1 suit
 			{
 				valueCount = 0;
-				suitCount++;
+				suitCount++;	//Then change suit and reset the value to go through iteration
 			}
 			else
-				valueCount++;
+				valueCount++;	//Iterate through value
 		}
 	}
+	
+	/**
+	 * Constructor
+	 * Fills empty array with the unshuffled deck of cards
+	 */
 	
 	public DeckOfCards()
 	{
