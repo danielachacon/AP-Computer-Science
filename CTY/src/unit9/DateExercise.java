78f336ee72bf91c4768d28c3601a4d0cb35d2dda
danielachacon <68338389+danielachacon@users.noreply.github.com>
2023-03-24 14:28:10 -0400

    Unit 9 Activity 1 test cases

--- CTY/src/unit9/DateExercise.java
+++ CTY/src/unit9/DateExercise.java
@@ -5,12 +5,20 @@ public class DateExercise {
 	public static void main(String[] args) 
 	{
 		Date date = new Date();
+		Date testDateMonth = new Date(2);
+		Date testDateNoParam = new Date();
+		Date testDateMonthDay = new Date (2,28);
+		Date testDateMonthDayYear = new Date(3,14,2002);
 		
 		date.setMonth(Input.getInt("Enter Month: "));
 		date.setDay(Input.getInt("Enter Day: "));
 		date.setyear(Input.getInt("Enter Year: "));
 		
 		date.displayDate();
+		testDateMonth.displayDate();
+		testDateNoParam.displayDate();
+		testDateMonthDay.displayDate();
+		testDateMonthDayYear.displayDate();
 	}
 
 }
