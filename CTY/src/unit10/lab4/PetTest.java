c3e6828249265f381e7d833225987468eed32e85
danielachacon <68338389+danielachacon@users.noreply.github.com>
2023-04-04 18:30:32 -0400

    commented PetTest

--- CTY/src/unit10/lab4/PetTest.java
+++ CTY/src/unit10/lab4/PetTest.java
@@ -18,17 +18,17 @@ public class PetTest
 	{
 		String s;
 		int petAmount = 3;
-		Pet petArray[] = new Pet[petAmount];
+		Pet petArray[] = new Pet[petAmount];	//Array of Pets
 		
 		for(int i = 0; i < petAmount; i++)
 		{	
 			System.out.print("Enter Type of Pet: ");
-			s = PetTest.getUserInput().toLowerCase();
+			s = PetTest.getUserInput().toLowerCase();	//Receives User Input of type of Pet
 			
 			if(s.equals("dog"))
-				petArray[i] = new Dog();
+				petArray[i] = new Dog();		//Creates a new Dog Object
 			else if(s.equals("cat"))
-				petArray[i] = new Cat();
+				petArray[i] = new Cat();	//Creates a new Cat Object
 			else
 			{
 				System.out.println("Input not Recognized");
@@ -36,15 +36,16 @@ public class PetTest
 			}
 			
 			System.out.print("Enter Name of Pet: ");
-			petArray[i].setName(PetTest.getUserInput());
+			petArray[i].setName(PetTest.getUserInput());	//Receive User Input of Name and stores it into current element of the array
 			System.out.print("Enter Weight of Pet: ");
-			petArray[i].setWeight(Integer.parseInt(PetTest.getUserInput()));
+			petArray[i].setWeight(Integer.parseInt(PetTest.getUserInput()));	//Receive User Input of Weight and stores it into current element of the array
 			System.out.print("Enter Age of Pet: ");
-			petArray[i].setAge(Integer.parseInt(PetTest.getUserInput()));
+			petArray[i].setAge(Integer.parseInt(PetTest.getUserInput()));	//Receive User Input of Age and stores it into current element of the array
 			
 			System.out.println();
 		}
 		
+		//Prints all elements of the Pet Array and their attributes 
 		for(int i = 0; i < petAmount; i++)
 		{
 			System.out.println("Pet " + (i + 1) + " Name: " + petArray[i].getName() + " Weight (lbs): " + petArray[i].getWeight() + " Age (years): " + petArray[i].getAge() + " Sound: " + petArray[i].sound());
