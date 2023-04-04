7bbdd191187d5ed4a5143d3d38f66f8a08c2bb30
danielachacon <68338389+danielachacon@users.noreply.github.com>
2023-04-04 18:44:10 -0400

    Commented CarTest

--- CTY/src/unit11/lab1/CarTest.java
+++ CTY/src/unit11/lab1/CarTest.java
@@ -3,6 +3,12 @@ package unit11.lab1;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * This class creates 3 ArrayLists and prompts the user to enter data for 5 Car objects and stores them in an ArrayList
+ * The program iterates over the list and stores any car with vehicle year earlier than 2010 into OldCar ArrayList and 
+ * cars with vehicle year >= 2010 into NewCar ArrayList
+ * @author Daniel Achacon
+ */
 public class CarTest {
 
 	public static void main(String[] args) 
@@ -13,25 +19,28 @@ public class CarTest {
 		ArrayList<Car> NewCars = new ArrayList<Car>();
 		Scanner input = new Scanner(System.in); 
 		
+		//Asks for attributes for amountOFCars amount of Cars
 		for(int i = 0; i < amountOfCars; i++)
 		{
 			Cars.add(new Car());
 			System.out.print("Enter Make for Car " + (i + 1) + ": ");
-			Cars.get(i).setMake(input.nextLine());
+			Cars.get(i).setMake(input.nextLine());	//Receives make user input
 			System.out.print("Enter Model for Car " + (i + 1) + ": ");
-			Cars.get(i).setModel(input.nextLine());
+			Cars.get(i).setModel(input.nextLine());	//Receives model user input
 			System.out.print("Enter year for Car " + (i + 1) + ": ");
-			Cars.get(i).setYear(Integer.parseInt(input.nextLine()));
+			Cars.get(i).setYear(Integer.parseInt(input.nextLine()));	//Receives year user input
 		}
 		
+		//Receives year user input
 		for(int i = 0; i < amountOfCars; i++)
 		{
-			if(Cars.get(i).getYear() >= 2010)
+			if(Cars.get(i).getYear() >= 2010)	//If the year attribute of the Car greater than or equal to 2010, then add it to NewCar Arraylist
 				NewCars.add(Cars.get(i));
 			else
-				OldCars.add(Cars.get(i));
+				OldCars.add(Cars.get(i));	//If the year attribute of the Car less than to 2010, then add it to OldCar Arraylist
 		}
 		
+		//Iterates through each element of the ArrayList Prints out all attributes of each element
 		System.out.println("\nArrayList of All Cars: ");
 		
 		for(int i = 0; i < amountOfCars; i++)
